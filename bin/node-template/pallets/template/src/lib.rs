dda63e709beae91731be7544f01f49b8a1cf32ce
prabal-banerjee <prabal.banerjee@matic.network>
2021-02-08 09:07:25 +0000

    Simple pallet to submit data as extrinsic - cannot submit same data twice

--- bin/node-template/pallets/template/src/lib.rs
+++ bin/node-template/pallets/template/src/lib.rs
@@ -23,11 +23,8 @@ pub trait Config: frame_system::Config {
 // The pallet's runtime storage items.
 // https://substrate.dev/docs/en/knowledgebase/runtime/storage
 decl_storage! {
-	// A unique name is used to ensure that the pallet's storage items are isolated.
-	// This name may be updated, but each pallet in the runtime must use a unique name.
-	// ---------------------------------vvvvvvvvvvvvvv
 	trait Store for Module<T: Config> as TemplateModule {
-		Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
+		HashToBytes: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
 	}
 }
 
@@ -35,22 +32,16 @@ decl_storage! {
 // https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as frame_system::Config>::AccountId {
-		/// Event emitted when a proof has been claimed. [who, claim]
-        ClaimCreated(AccountId, Vec<u8>),
-        /// Event emitted when a claim is revoked by the owner. [who, claim]
-        ClaimRevoked(AccountId, Vec<u8>),
+		/// Event emitted when a data has been submitted. [who, data]
+        DataSubmitted(AccountId, Vec<u8>),
 	}
 );
 
 // Errors inform users that something went wrong.
 decl_error! {
 	pub enum Error for Module<T: Config> {
-		/// The proof has already been claimed.
-        ProofAlreadyClaimed,
-        /// The proof does not exist, so it cannot be revoked.
-        NoSuchProof,
-        /// The proof is claimed by another account, so caller can't revoke it.
-        NotProofOwner,
+		/// The data has already been submitted.
+        DataAlreadySubmitted,
 	}
 }
 
@@ -65,50 +56,28 @@ decl_module! {
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
 
-		/// Allow a user to claim ownership of an unclaimed proof.
+		/// Allow a user to submit new data.
         #[weight = 10_000]
-        fn create_claim(origin, proof: Vec<u8>) {
+        fn submit_data(origin, data: Vec<u8>) {
             // Check that the extrinsic was signed and get the signer.
             // This function will return an error if the extrinsic is not signed.
             // https://substrate.dev/docs/en/knowledgebase/runtime/origin
             let sender = ensure_signed(origin)?;
 
-            // Verify that the specified proof has not already been claimed.
-            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+            // Verify that the given data has not already been submitted.
+            ensure!(!HashToBytes::<T>::contains_key(&data), Error::<T>::DataAlreadySubmitted);
 
             // Get the block number from the FRAME System module.
             let current_block = <frame_system::Module<T>>::block_number();
 
-            // Store the proof with the sender and block number.
-            Proofs::<T>::insert(&proof, (&sender, current_block));
+            // Store the data with the sender and block number.
+            HashToBytes::<T>::insert(&data, (&sender, current_block));
 
             // Emit an event that the claim was created.
-            Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
+            Self::deposit_event(RawEvent::DataSubmitted(sender, data));
         }
 
-        /// Allow the owner to revoke their claim.
-        #[weight = 10_000]
-        fn revoke_claim(origin, proof: Vec<u8>) {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
-            let sender = ensure_signed(origin)?;
-
-            // Verify that the specified proof has been claimed.
-            ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
-
-            // Get owner of the claim.
-            let (owner, _) = Proofs::<T>::get(&proof);
-
-            // Verify that sender of the current call is the claim owner.
-            ensure!(sender == owner, Error::<T>::NotProofOwner);
-
-            // Remove claim from storage.
-            Proofs::<T>::remove(&proof);
-
-            // Emit an event that the claim was erased.
-            Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
-        }
+        
 
 	}
 }
