cfaaaed76d81d98808b3dafb8835e9132bac304a
Aleksandar Terentić <aleksandar@ethernal.tech>
2022-01-30 22:47:28 +0100

    Add compile time feature flag for column/row extension.

--- bin/node/cli/src/chain_spec.rs
+++ bin/node/cli/src/chain_spec.rs
@@ -245,13 +245,15 @@ pub fn testnet_genesis(
 
 	const ENDOWMENT: Balance = 10_000_000 * DOLLARS;
 	const STASH: Balance = ENDOWMENT / 1000;
+	const BLOCK_ROWS: u32  = if cfg!(feature = "extend-columns") { 128 } else { 256 };
+	const BLOCK_COLUMNS: u32  = if cfg!(feature = "extend-columns") { 512 } else { 256 };
 
 	GenesisConfig {
 		frame_system: Some(SystemConfig {
 			code: wasm_binary_unwrap().to_vec(),
 			changes_trie_config: Default::default(),
 			kc_public_params: kate::testnet::KC_PUB_PARAMS.to_vec(), 
-			block_length: BlockLength::with_normal_ratio(128, 256, 32, Perbill::from_percent(90)),
+			block_length: BlockLength::with_normal_ratio(BLOCK_ROWS, BLOCK_COLUMNS, 32, Perbill::from_percent(90)),
 		}),
 		pallet_balances: Some(BalancesConfig {
 			balances: endowed_accounts.iter().cloned()
